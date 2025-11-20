80dd7e405a4071571e09a3d02812c3452d602a5e
Maximiliano Pizarro <maximiliano.pizarro.5@gmail.com>
2025-11-20 16:44:43 -0300

    fix empty default value from frontend

--- neuralbank-front/src/constants/api.ts
+++ neuralbank-front/src/constants/api.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://neuralbank-neural-bank-demo.apps.cluster-4n6sx.4n6sx.sandbox2864.opentlc.com";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
